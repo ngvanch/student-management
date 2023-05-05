2886bff531601c663b2cc3595a55dc55af6c531f
unknown <Administrator@VIP-06.VIP-02>
2023-05-05 15:53:41 +0700

    API backend signup / signin (chua hoan thanh)

---
+++ index.js
@@ -0,0 +1,34 @@
+const port = 9000;
+const express = require('express');
+const app = express();
+const cors = require("cors");
+const User = require('../database/User');
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, resp) => {
+    resp.send("Hello World");
+})
+
+app.post("/register", async (req, resp) => {
+    let user = new User(req.body);
+    let result = await user.save();
+    resp.send(result);
+})
+
+app.post("/login", async (req, resp) => {
+    if (req.body.password && req.body.email) {
+        let user = await User.findOne(req.body).select("-password");
+        if (user) {
+            resp.send(user);
+        }
+        else {
+            resp.send({ result: "No User found" });
+        }
+    }
+})
+
+app.listen(port, () => {
+    console.log("App is listening port " + port);
+})
