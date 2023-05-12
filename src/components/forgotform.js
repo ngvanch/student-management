0df80acedaba602e2d2fe98c1e3dc4e4290bab19
Bé Dâu Dâu <107912469+BeDauDau@users.noreply.github.com>
2023-05-12 18:38:16 +0700

    add new code
    add new code

---
+++ src/components/forgotform.js
@@ -0,0 +1,38 @@
+import React, {useEffect, useState} from "react";
+import Popup from 'reactjs-popup';
+import { Outlet, Link, Route, Routes } from "react-router-dom";
+import "./registerform.css"
+
+const ForgotForm = () => {
+
+
+    const [popupStyle, showPopup] = useState("hide")
+    const popup = () => {
+        showPopup("login-popup")
+        setTimeout(() => showPopup("hide"), 3000)
+    }
+
+    return (
+        <div className="page">
+            <div className="cover">
+
+                <h1>forgot password</h1>
+                {/* */}
+                <input type="text" required placeholder="username" />
+
+                <input type="password" required placeholder="password" />
+
+                <input type="password" required placeholder="Confirm Password" />
+
+                {/*Login Button */}
+
+                <div className="login-btn" onClick={popup}>Reset</div>
+                <Link to="/login" className="login-btn">exit</Link>
+
+            </div>
+            <Outlet />
+        </div>
+    )
+}
+
+export default ForgotForm
