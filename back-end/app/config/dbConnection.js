c63544100c27c1c6bbf21b4de3153bbbf46122e3
Notistris <mizavayx@gmail.com~>
2023-05-19 20:00:39 +0700

    Add env variables

--- back-end/app/config/dbConnection.js
+++ back-end/app/config/dbConnection.js
@@ -1,17 +1,17 @@
 var mysql = require("mysql");
 var conn = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "QLHS",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 conn.connect((err) => {
   if (err) throw err;
-  // if error run command:
+  // if error.code = 'ER_NOT_SUPPORTED_AUTH_MODE', run command:
   // ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '';
   // in MySQL
-  console.log("Database is connected successfully !");
+  console.log("Database is connected successfully!");
 });
 
 module.exports = conn;
